47da05a89dd45d290110ae23913ae0d4c5be2514
chapetta <cha.petta@hotmail.com>
2024-04-12 17:36:41 -0300

    criando a funçao fetch cambio

---
+++ api.js
@@ -0,0 +1,16 @@
+import fetch from "node-fetch"
+
+async function cambioAPI (moeda) {
+  const API_KEY = 'c8da5cbb56eea923e8ddb645'
+  const ENDPOINT = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${moeda}`
+  const response = await fetch(ENDPOINT)
+  const {base_code, conversion_rates} = await response.json()
+  return {
+    base_code,
+    conversion_rates
+  }
+  
+  
+}
+
+export default cambioAPI;
