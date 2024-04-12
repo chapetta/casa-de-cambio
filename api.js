f6d821ca846c2509e9c093da20038015306c89b8
chapetta <cha.petta@hotmail.com>
2024-04-12 17:37:02 -0300

    mudando o nome da api

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 import fetch from "node-fetch"
 
-async function cambioAPI (moeda) {
+async function fetchCurrencies (moeda) {
   const API_KEY = 'c8da5cbb56eea923e8ddb645'
   const ENDPOINT = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${moeda}`
   const response = await fetch(ENDPOINT)
