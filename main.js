c3b8ca3254bb3082ea898971514ac66c3ef18160
chapetta <cha.petta@hotmail.com>
2024-04-15 15:00:09 -0300

    implementando as funçoes de get currencie para pegar os dados

--- main.js
+++ main.js
@@ -1,13 +1,51 @@
 import fetchCurrencies from "./api"
 
-async function start() {
-  const { base_code, conversion_rates } = await fetchCurrencies('BRL')
+
+function renderRAte(currencies, rate) {
   const ul = document.querySelector('ul')
-  const ratesEntries = Object.entries(conversion_rates)
-  ratesEntries.forEach((entry) => {
-    const li = document.createElement('li')
-    li.innerHTML = `${entry[0]}: ${entry[1]}`
+  const li = document.createElement('li')
+    li.innerText = `${currencies}: ${rate}`
     ul.appendChild(li)
+}
+
+function handleRates(rates) {
+  const ratesEntries = Object.entries(rates)
+  ratesEntries.forEach((entry) => {
+    renderRAte(entry[0], entry[1])
   })
 }
+import Swal from "sweetalert2"
+
+function handleBase(base) {
+  const baseElement = document.querySelector('#base')
+  baseElement.innerText = `Valores referentes a 1 ${base}:`
+}
+
+function getCurrencies() {
+  const searchInput = document.querySelector('#currency-input')
+  const searchButton = document.querySelector('#search-button')
+
+  searchButton.addEventListener('click', async () => {
+    try {
+      const currencie = searchInput.value
+      const { base_code, conversion_rates } = await fetchCurrencies(currencie)
+      handleRates(conversion_rates)
+      handleBase(base_code)
+      searchInput.value = ''
+    } catch(error) {
+      Swal.fire({
+        title: 'Ops',
+        text: 'Moeda incorreta! Tente novamente',
+        icon: 'error'
+    })
+    searchInput.value = ''
+    }
+  })
+}
+
+async function start() {
+  getCurrencies()
+}
+
+start()
 
